39fa9881082175da28e45c1a7011933709a24522
Khanjan Vaidya <47827803+khanjan2000@users.noreply.github.com>
2019-09-05 23:06:54 +0530

    This is the code used for encryption using ASCII

---
+++ DECRYPT(ASCII).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+   int i, x;
+   char str[100];
+
+   printf("\nPlease enter a string\t");
+   gets(str);
+
+  
+      for(i = 0; (i < 100 && str[i] != '\0'); i++)
+        str[i] = str[i] - 3; //in this 3 is subtracted to the ascii value.
+
+      printf("\nDecyrpted string: %s\n", str);
+   return 0;
+}
